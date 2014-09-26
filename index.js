75fb380f609c49ca85b27d99ca7d76558b3010b1
yufei <googleyufei@qq.com>
2014-09-26 15:25:58 +0800

    'update'

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var handlebars = require('handlebars');
 
-module.exports = function(content, file, opt){
-    content = handlebars.precompile(content, opt);
+module.exports = function (content, file, opt) {
+    if (!file.isHbsFile) {
+        return content;
+    }
+    content = handlebars.precompile(content.replace(/[\r\r]+/, ""), opt);
     return 'Handlebars.template(' + content + ')';
 };

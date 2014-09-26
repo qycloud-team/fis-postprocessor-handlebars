4df829b264af859478808a70e019f961096ddf47
yufei <googleyufei@qq.com>
2014-09-26 16:18:04 +0800

    update regexp

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ module.exports = function (content, file, opt) {
     if (!file.isHbsFile) {
         return content;
     }
-    content = handlebars.precompile(content.replace(/[\r\r]+/, ""), opt);
+    content = handlebars.precompile(content.replace(/[\r\n]+\s*/gm, ""), opt);
     return 'Handlebars.template(' + content + ')';
 };
